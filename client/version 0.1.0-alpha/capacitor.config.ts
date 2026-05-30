eec165dfd8b7467931201ca30fb97d480154c3e3
neiroset-7373 <2015nikmayboroda2015@bk.ru>
2026-05-30 14:12:19 +0300

    Add complete mobile/static build for Android app - separate from Flask PC version

--- client/version 0.1.0-alpha/capacitor.config.ts
+++ client/version 0.1.0-alpha/capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'org.wintozo.wintoblox',
   appName: 'WintoBlox - Alpha Version',
-  webDir: 'static'
+  webDir: 'mobile-static'
 };
 
 export default config;
